434261756ab54d0d18cbc0280d98d751fee824e2
bamboer <bamboer@bamboerdeMacBook-Pro.local>
2022-05-25 00:25:24 +0800

    change part size

--- internal/service/client.go
+++ internal/service/client.go
@@ -12,7 +12,7 @@ import (
 
 // DefaultDownloadPartSize is the default range of bytes to get at a time when
 // using Download().
-const DefaultDownloadPartSize = 1024 * 1024 * 5
+const DefaultDownloadPartSize = 1024 * 1024 * 168
 
 // DefaultDownloadConcurrency is the default number of goroutines to spin up
 // when using Download().
